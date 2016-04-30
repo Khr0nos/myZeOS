228be9979d16221e894d6f3788537789c5f36712
Khr0nos <antiwindows17@gmail.com>
2016-04-30 22:06:20 +0200

    neteja comments

--- zeos/user.c
+++ zeos/user.c
@@ -58,7 +58,7 @@ int workload_blocks() {
   for (i = 0; i < 10; ++i) {
     int n = read(0, buf, 2000);
     if (n < 0) perror();
-    else write(1, "OK", strlen("OK"));
+    else write(1, "\nOK", strlen("\nOK"));
   }
   return i;
 }
@@ -68,51 +68,8 @@ int __attribute__ ((__section__(".text.main")))
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
-    /*int ret = write(1, "\n\nProva de write", strlen("\n\nProva de write"));
-    if (ret < 0) perror();
-    int p = getpid();
-    if (p < 0) perror();
-    itoa(p,buff);
-    write(1,"\nProces amb PID ",strlen("\nProces amb PID "));
-    write(1,buff, strlen(buff));*/
-    //runjp();
-    //runjp_rank(5,31);
-    /*pid = fork();
-    if (pid < 0) perror();
-    if (pid == 0) {
-      pid = getpid();
-      itoa(pid, buff);
-      write(1,"\nEl meu PID es ",strlen("\nEl meu PID es "));
-      write(1,buff, strlen(buff));
-      pid = fork();
-      if (pid < 0) perror();
-      if (pid == 0) {
-        pid = getpid();
-        itoa(pid, buff);
-        write(1,"\nEl meu PID es ",strlen("\nEl meu PID es "));
-        write(1,buff, strlen(buff));
-        exit();
-      } else {
-        itoa(pid, buff);
-        write(1,"\nEl PID del net es ",strlen("\nEl PID del net es "));
-        write(1,buff, strlen(buff));
-        exit();
-      }
-    } else {
-      if (pid < 0) perror();
-      workload_CPU();
-      print_stats(getpid());
-      itoa(pid, buff);
-      write(1,"\nEl PID del meu fill es ",strlen("\nEl PID del meu fill es "));
-      write(1,buff, strlen(buff));
-      pid = getpid();
-      itoa(pid, buff);
-      write(1,"\nEl meu PID es ",strlen("\nEl meu PID es "));
-      write(1,buff, strlen(buff));
-    }*/
     pid = set_sched_policy(RR);
     if (pid < 0) perror();
-
 
     pid = fork();
     if (pid < 0) perror();
@@ -124,16 +81,10 @@ int __attribute__ ((__section__(".text.main")))
         write(1, "\n", strlen("\n"));
         write(1, buff, strlen(buff));
         write(1, "\n", strlen("\n"));
-        //int n = read(0, buf, 2000);
-        //if (n < 0) perror();
-        //else write(1, "\nchild Read OK\n", strlen("\nchild Read OK\n"));
         print_stats(getpid());
         exit();
       } else {
-        //int n = read(0, buf, 100);
-        //if (n < 0) perror();
-        //else write(1, "\nfather Read OK\n", strlen("\nfather Read OK\n"));
-        int f = workload_blocks();
+        int f = workload_CPU();
         itoa(f, buff);
         write(1, buff, strlen(buff));
         write(1, "\n", strlen("\n"));
